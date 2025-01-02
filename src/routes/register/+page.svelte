a83106a7dd88164f13b303dc285cac89073aee12
Prakit <saratgogoi78@gmail.com>
2025-01-02 17:32:33 +0530

    Update +page.svelte

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -30,7 +30,7 @@ async function register() {
 
 <main>
     <div class="container">
-         <img src="/CloudSapphire-Neo.png" alt="Logo" class="logo" />
+         <img src="/CloudSapphire-Neo.png" alt="Cloudsapphire Neo" class="logo" />
         <h1>Register</h1>
 
         <form on:submit|preventDefault={register}>
